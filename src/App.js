ca06913ae5818406a079d7b16a8ce8b0b6dea491
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-04-17 16:40:53 +0530

    setting up the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 function App() {
-  return <h1>Hello from app component</h1>;
+  const title = "hello from App.js";
+  const body = "this is another variable";
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "comment two" },
+    { id: 3, text: "comment three" },
+  ];
+  const showComments = true;
+
+  const commentBlock = (
+    <div className="comments">
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  return (
+    <div className="Container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      {showComments && commentBlock}
+    </div>
+  );
 }
 
 export default App;
