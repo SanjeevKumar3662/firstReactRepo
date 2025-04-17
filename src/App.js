fd601ddb97cc653c74e960241509764f3acc737e
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-04-17 18:45:57 +0530

    re-setting the app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
+import Header from "./components/Header";
+
 function App() {
-  const title = "hello from App.js";
-  const body = "this is another variable";
-  const comments = [
-    { id: 1, text: "Comment one" },
-    { id: 2, text: "comment two" },
-    { id: 3, text: "comment three" },
-  ];
-  const showComments = true;
-
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
   return (
-    <div className="Container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
-    </div>
+    <>
+      <Header text="Hello Feedback UI" />
+      <div className="Container">
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 
